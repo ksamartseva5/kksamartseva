2d1acb6c1eb9c83475dc16e4bdfa44fc7a8d3f98
ksamartseva5 <Ksenia.Samartseva@urfu.me>
2021-10-28 13:44:05 +0500

    первое задание выполнено

---
+++ task1/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.ReadKey();
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("Николай Гумилев. На горах розовеют снега...");
+			Console.ReadKey();
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("На горах розовеют снега,");
+			Console.WriteLine("Я грущу с каждым мигом сильней,");
+			Console.WriteLine("Для кого я сбирал жемчуга");
+			Console.WriteLine("В зеленеющей бездне морей ?");
+			Console.ResetColor();
+		}
+    }
+}
