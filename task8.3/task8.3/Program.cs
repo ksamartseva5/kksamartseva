398f834b68c29fde58cb17285404fdf873f1440f
ksamartseva5 <Ksenia.Samartseva@urfu.me>
2021-12-28 14:12:09 +0500

    задание 8.3 частично выполнено

---
+++ task8.3/task8.3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите целое число");
+            var a = int.Parse(Console.ReadLine());
+
+            int sum = a;
+            int multiply = a;
+
+            if (a > 0)
+            {
+                while (a != 0)
+                {
+                    Console.WriteLine("Введите целое число");
+                    a = int.Parse(Console.ReadLine());
+                    sum += a;
+                }
+                Console.WriteLine(sum);
+            }
+            else
+            {
+                while (a != 0)
+                {
+                    Console.WriteLine("Введите целое число");
+                    a = int.Parse(Console.ReadLine());
+                    multiply *= a;
+
+                }
+                Console.WriteLine(multiply);
+            }
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
