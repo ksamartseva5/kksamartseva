326aab5afeeaa2285b42f68fa664a868e2186ee6
ksamartseva5 <Ksenia.Samartseva@urfu.me>
2021-12-28 13:55:29 +0500

    task 8.5 выполено

---
+++ task8.5/task8.5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число");
+            var number = int.Parse(Console.ReadLine());
+
+            var divider = 1;
+
+            for (var i = 2; i < number; i++)
+                if (number % i == 0)
+                {
+                    divider = i;
+                    break;
+                }
+            var maxdivider = number / divider;
+
+            if (maxdivider == number)
+                Console.WriteLine("так как это простое число, макс. делителя нет.");
+            else
+            Console.WriteLine($"Максимальный делитель {maxdivider}");
+            Console.ReadKey();
+                   
+
+                
+               
+        }
+    }
+}
