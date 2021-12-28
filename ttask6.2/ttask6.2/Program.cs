7446b5d10e01ced27c5d87c5d07d8bf0686001c1
ksamartseva5 <Ksenia.Samartseva@urfu.me>
2021-12-28 13:11:31 +0500

    задание 6.2 выполнено

---
+++ ttask6.2/ttask6.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ttask6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var x = InputCoordinate("абсциссу");
+                var y = InputCoordinate("ординату");
+
+                Console.WriteLine($"Точка внутри области " + IsInsideArea(x, y));
+
+                Console.ReadKey();
+            }
+
+             bool IsInsideArea(double x, double y)
+            {
+                return y>=-0.5 && y<=2 && x>=-1 && x<=1.5;
+            }
+
+          double InputCoordinate(string name)
+            {
+                Console.WriteLine($"Введите {name} точки");
+                return double.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
