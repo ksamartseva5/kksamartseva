7316f0a6fd813f46b0e0f98958244e37bf82a11a
ksamartseva5 <Ksenia.Samartseva@urfu.me>
2021-12-28 13:29:36 +0500

    задание 8.1 выполнено

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое целое положительное число");
+            var a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе целое число, большее или равное первому");
+            var b = int.Parse(Console.ReadLine());
+
+            int multiply = 1;
+
+            for (var i = a; i <= b; i++)
+                multiply *= i;
+
+            Console.WriteLine($"Результат:{multiply}");
+            Console.ReadKey();
+        }
+    }
+}
