9124b53aae87e39754309b16649b573a7aee5fa0
ksamartseva5 <Ksenia.Samartseva@urfu.me>
2023-06-13 10:45:59 +0500

    задание 6 выполнено

---
+++ 6 задание/UGI-216004-main/PhotoEnhancer/PhotoEnhancer/Filters/Transform/BevelUpParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class BevelUpParameters : IParameters
+    {
+        [ParameterInfo(Name = "Угол в °",
+                    MinValue = 0,
+                    MaxValue = 85,
+                    DefaultValue = 0,
+                    Increment = 5)]
+        public double AngleInDegrees { get; set; }
+    }
+}
